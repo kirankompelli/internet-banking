76c9041a5de36d1407e92eb04f8c9647149dc27b
Kiran Kompelli <kompellikiran15241@gmail.com>
2025-07-24 11:51:46 +0530

    new

--- src/main/java/com/kiran/InternetBankingApplication.java
+++ src/main/java/com/kiran/InternetBankingApplication.java
@@ -8,6 +8,7 @@ public class InternetBankingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InternetBankingApplication.class, args);
+		System.out.println("new project");
 	}
 
 }
